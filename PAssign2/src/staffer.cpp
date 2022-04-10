c08d7ef4260617e7de686e8160ef3031fdc1ef41
xTechon <DanielHiromoto@outlook.com>
2022-04-10 18:56:45 -0400

    Safety Function Comps passes init tests
     - Safety function correctly identifies when Needi < Work
     - Safety funciton incomplete still

--- PAssign2/src/staffer.cpp
+++ PAssign2/src/staffer.cpp
@@ -33,11 +33,36 @@ int Banker::checkSafety() {
   // let work and finish be vector of length m,n resp
   Bank::vector_t work = this->Avail;
   std::vector<bool> Finish;
+  std::cout << this->getRows() << std::endl;
   Finish.reserve(this->getRows());
-  std::fill(Finish.begin(), Finish.end(), (bool)false);
-  std::cout << "FINISH VECTOR MADE" << std::endl;
+  for (int i = 0; i < this->getRows(); i++)
+    Finish.push_back(false);
+  // std::cout << "FINISH VECTOR MADE: " << Finish.size() << std::endl;
+  /*
   for (auto i = Finish.begin(); i != Finish.end(); ++i)
     std::cout << *i << " ";
   std::cout << std::endl;
+  */
+  // Find an i such that both
+  // finish[i] = false
+  // needi < work
+  // if no such i exists, go to step 4
+  int j = 0;
+  Bank::vector_t temp = (Bank::vector_t)(this->Need.row(j));
+  Bank::vector_t res;
+  for (auto i = Finish.begin(); i != Finish.end(); ++i) {
+    // convert from one row to a vector
+    temp = (Bank::vector_t)(this->Need.row(j));
+    // hopefully store the comparison
+    res = work - temp;
+    std::cout << "WORK - NEEDi = " << res << std::endl;
+    // If Needi < Work, than Work - Needi > 0 for all values
+    if ((*i == false) && (res.minCoeff() >= 0)) {
+      std::cout << "CONDITION PASSED" << std::endl;
+    } else {
+      std::cout << "CONDITION FAILED" << std::endl;
+    }
+    j++;
+  }
   return 0;
 }
